27c3e87bdfeed00c54c909c06bacd918a081c59b
Vivek Bhand <68386459+VivekBhand@users.noreply.github.com>
2021-04-27 11:53:59 +0530

    Add files via upload

--- employee.py
+++ employee.py
@@ -1,114 +1,160 @@
 from tkinter import *
+import tkinter.messagebox
 import sqlite3
 
-def employee():
-    global root3
-    root3 = Tk()
-    root3.geometry("600x800+720+0")
-    root3.title("RECORDS")
-    Employeename = StringVar()
-    Employeeid = StringVar()
-    Mobileno = StringVar()
-    Email = StringVar()
-    Aadhar = StringVar()
-    Age = StringVar()
-    Salary = StringVar()
-    var = IntVar()
-    s = StringVar()
+def func5():
+    conn = sqlite3.connect('EMPLOYEE APPOINTMENT RECORDS.db')
+    c = conn.cursor()
+
+    class Application:
+            def __init__(self, master):
+                self.master = master
+
+                self.heading = Label(master, text="Update or Delete Records",  fg='steelblue', font=('arial 20'))
+                self.heading.place(x=130, y=0)
 
 
-    def edatabase():
-        employeename = Employeename.get()
-        employeeid = Employeeid.get()
-        mobile = Mobileno.get()
-        email = Email.get()
-        aadhar = Aadhar.get()
-        age = Age.get()
-        salary = Salary.get()
-        gender = var.get()
-        employeetype = s.get()
-        conn = sqlite3.connect('EMPLOYEE APPOINTMENT RECORDS.db')
-        with conn:
-            cursor = conn.cursor()
-        cursor.execute(
-            'CREATE TABLE IF NOT EXISTS Employee (Employeename TEXT,Employeeid TEXT,Mobileno TEXT,Email TEXT,Aadhar TEXT,Age TEXT,Salary TEXT,Gender TEXT,Employeetype TEXT)')
-        cursor.execute(
-            'INSERT INTO Employee (Employeename,Employeeid,Mobileno,Email,Aadhar,Age,Salary,Gender,Employeetype) VALUES(?,?,?,?,?,?,?,?,?)',
-            (employeename, employeeid, mobile, email, aadhar, age, salary, gender, employeetype,))
-        conn.commit()
+                self.name = Label(master, text="Enter Staff's ID", font=('arial 13'))
+                self.name.place(x=130, y=60)
 
 
-    def reg():
+                self.namenet = Entry(master, width=30)
+                self.namenet.place(x=350, y=62)
 
-        label_0 = Label(root3, text="EMPLOYEE APPOINTMENT RECORDS", fg='red', width=0, font=("bold", 15))
-        label_0.place(x=90, y=53)
 
-        label_1 = Label(root3, text="ENTER EMPLOYEE NAME", width=20, font=("bold", 9))
-        label_1.place(x=80, y=130)
+                self.search = Button(master, text="Search", width=12, height=1, bg='steelblue', command=self.search_db)
+                self.search.place(x=400, y=102)
 
-        entry_1 = Entry(root3, textvar=Employeename)
-        entry_1.place(x=240, y=130)
+            def search_db(self):
+                self.input = self.namenet.get()
 
-        label_2 = Label(root3, text="ENTER EMPLOYEE ID", width=20, font=("bold", 9))
-        label_2.place(x=80, y=155)
 
-        entry_2 = Entry(root3, textvar=Employeeid)
-        entry_2.place(x=240, y=155)
+                sql = "SELECT * FROM Employee WHERE Employeeid LIKE ?"
+                self.res = c.execute(sql, (self.input,))
+                for self.row in self.res:
 
-        label_3 = Label(root3, text="ENTER MOBILE NO.", width=20, font=("bold", 9))
-        label_3.place(x=80, y=180)
+                    if (self.row[1] == self.input ):
+                        self.ename = self.row[0]
+                        self.eid = self.row[1]
+                        self.emobile = self.row[2]
+                        self.email  = self.row[3]
+                        self.eadhar = self.row[4]
+                        self.age = self.row[5]
+                        self.salary = self.row[6]
+                        self.gender = self.row[7]
+                        self.etype = self.row[8]
 
-        entry_3 = Entry(root3, textvar=Mobileno)
-        entry_3.place(x=240, y=180)
 
-        label_4 = Label(root3, text="ENTER EMAIL", width=20, font=("bold", 9))
-        label_4.place(x=80, y=205)
 
-        entry_4 = Entry(root3, textvar=Email)
-        entry_4.place(x=240, y=205)
+                        self.uname = Label(self.master, text="Staff's Name", font=('arial 13'))
+                        self.uname.place(  x=130, y=140)
 
-        label_5 = Label(root3, text="ENTER AADHAR NO.", width=20, font=("bold", 9))
-        label_5.place(x=80, y=230)
+                        self.umobile = Label(self.master, text="Employee ID.", font=('arial 13 '))
+                        self.umobile.place(  x=130, y=180)
 
-        entry_5 = Entry(root3, textvar=Aadhar)
-        entry_5.place(x=240, y=230)
+                        self.uemail = Label(self.master, text="Mobile No", font=('arial 13  '))
+                        self.uemail.place(  x=130, y=220)
 
-        label_6 = Label(root3, text="AGE", width=20, font=("bold", 9))
-        label_6.place(x=80, y=255)
+                        self.uaadhar = Label(self.master, text="Email", font=('arial 13  '))
+                        self.uaadhar.place(  x=130, y=260)
 
-        entry_6 = Entry(root3, textvar=Age)
-        entry_6.place(x=240, y=255)
+                        self.udate = Label(self.master, text="Aadhar", font=('arial 13  '))
+                        self.udate.place(  x=130, y=300)
 
-        label_7 = Label(root3, text="SALARY", width=20, font=("bold", 9))
-        label_7.place(x=80, y=280)
+                        self.uage = Label(self.master, text="Age", font=('arial 13  '))
+                        self.uage.place(  x=130, y=340)
 
-        entry_7 = Entry(root3, textvar=Salary)
-        entry_7.place(x=240, y=280)
+                        self.ugender = Label(self.master, text="Salary", font=('arial 13  '))
+                        self.ugender.place(  x=130, y=380)
 
-        label_8 = Label(root3, text="SELECT GENDER", width=20, font=("bold", 9))
-        label_8.place(x=80, y=305)
+                        self.usymptoms = Label(self.master, text="Gender", font=('arial 13  '))
+                        self.usymptoms.place(  x=130, y=420)
 
-        r1 = Radiobutton(root3, text="MALE", padx=5, variable=var, value=1)
-        r1.place(x=235, y=305)
-        r2 = Radiobutton(root3, text="FEMALE", padx=20, variable=var, value=2)
-        r2.place(x=300, y=305)
+                        self.uvaccine = Label(self.master, text="Employeetype", font=('arial 13  '))
+                        self.uvaccine.place(  x=130, y=460)
 
-        label_9 = Label(root3, text="SELECT EMPLOYEE TYPE", width=20, font=("bold", 9))
-        label_9.place(x=70, y=330)
 
-        list12 = ['Doctor', 'Nurse', 'Floor sweeper'];
 
-        droplist = OptionMenu(root3, s, *list12)
-        droplist.config(bg='yellow', fg='black', width=15)
-        s.set('Select Employee Type')
-        droplist.place(x=240, y=330)
+                        self.ent0 = Entry(self.master, width=30)
+                        self.ent0.place( x=300, y=140)
+                        self.ent0.insert(END, str(self.ename))
 
-        Button(root3, text='Submit', width=20, bg='green', fg='black', command=edatabase).place(x=180, y=440)
-        Button(root3, text='Exit', width=20, bg='green', fg='black', command=exit).place(x=180, y=480)
+                        self.ent1 = Entry(self.master, width=30)
+                        self.ent1.place( x=300, y=180)
+                        self.ent1.insert(END, str(self.eid))
 
-        root3.mainloop()
-    def exit():
-        global root3
-        root3.destroy()
+                        self.ent2 = Entry(self.master, width=30)
+                        self.ent2.place(x=300, y=220)
+                        self.ent2.insert(END, str(self.emobile))
 
-    reg()
+                        self.ent3 = Entry(self.master, width=30)
+                        self.ent3.place( x=300, y=260)
+                        self.ent3.insert(END, str(self.email))
+
+                        self.ent4 = Entry(self.master, width=30)
+                        self.ent4.place( x=300, y=300)
+                        self.ent4.insert(END, str(self.eadhar))
+
+                        self.ent5 = Entry(self.master, width=30)
+                        self.ent5.place( x=300, y=340)
+                        self.ent5.insert(END, str(self.age))
+
+                        self.ent6= Entry(self.master, width=30)
+                        self.ent6.place( x=300, y=380)
+                        self.ent6.insert(END, str(self.salary))
+
+                        self.ent7 = Entry(self.master, width=30)
+                        self.ent7.place( x=300, y=420)
+                        self.ent7.insert(END, str(self.gender))
+
+                        self.ent8 = Entry(self.master, width=30)
+                        self.ent8.place( x=300, y=460)
+                        self.ent8.insert(END, str(self.etype))
+
+
+
+
+                        self.update = Button(self.master, text="Update", width=20, height=2, bg='lightblue', command=self.update_db)
+                        self.update.place(x=400, y=580)
+
+
+                        self.delete = Button(self.master, text="Delete", width=20, height=2, bg='red', command=self.delete_db)
+                        self.delete.place(x=130, y=580)
+                    else:
+                        tkinter.messagebox.showinfo("Error", "NOT FOUND")
+            def update_db(self):
+
+                self.var1 = self.ent1.get()
+                self.var2 = self.ent2.get()
+                self.var3 = self.ent3.get()
+                self.var4 = self.ent4.get()
+                self.var5 = self.ent5.get()
+                self.var6 = self.ent6.get()
+                self.var7 = self.ent7.get()
+                self.var8 = self.ent8.get()
+
+
+                query = "UPDATE Employee SET Employeename = ?,Employeeid = ?,Mobileno = ?,Email = ?,Aadhar = ?,Age = ?,Salary = ?,Gender = ?,Employeetype = ? WHERE Fullname LIKE ?"
+                c.execute(query, (self.var1, self.var2, self.var3, self.var4, self.var5, self.var6,self.var7,self.var8,self.var9,self.var10,self.var11, self.namenet.get(),))
+                conn.commit()
+                tkinter.messagebox.showinfo("Updated", "Successfully Updated.")
+            def delete_db(self):
+
+                sql2 = "DELETE FROM Employee WHERE Fullname LIKE ?"
+                c.execute(sql2, (self.namenet.get(),))
+                conn.commit()
+                tkinter.messagebox.showinfo("Success", "Deleted Successfully")
+                self.ent1.destroy()
+                self.ent2.destroy()
+                self.ent3.destroy()
+                self.ent4.destroy()
+                self.ent5.destroy()
+                self.ent6.destroy()
+                self.ent7.destroy()
+                self.ent8.destroy()
+
+
+    root1 = Tk()
+    b = Application(root1)
+    root1.geometry("600x800+600+0")
+    root1.mainloop()
--- menu.py
+++ menu.py
@@ -2,21 +2,9 @@ import tkinter
 import sqlite3
 import tkinter.messagebox
 from appointement import *
+from employee import *
 from update import *
-#variables
-root1=None
-rootp=None
-pat_ID=None
-pat_name=None
-pat_dob=None
-pat_address=None
-pat_sex=None
-pat_BG=None
-pat_email=None
-pat_contact=None
-pat_contactalt=None
-pat_CT=None
-window=None
+
 
 #EXIT for MENU
 def ex():
@@ -32,19 +20,23 @@ def menu():
     root1.title("MAIN MENU")
     m=tkinter.Label(root1,text="MENU",font='Times 16 bold italic',fg='grey')
     button1=tkinter.Button(root1,text="1. BOOK APPOINTMENT",command=func,bg='light blue',fg='black')
-    
-    button3 = tkinter.Button(root1, text="2.EMPLOYEE REGISTRATION",command=reg,bg='light blue',fg='black')
-    button4 = tkinter.Button(root1, text="3.DELETE or UPDATE APPOINTMENT",command=func4,bg='light green',fg='black')
-    
-    button6 = tkinter.Button(root1, text="4.EXIT",command=ex,bg='light green',fg='black')
+    button2 = tkinter.Button(root1, text="2.VIEW APPOINTMENTS",command=func4,bg='light green',fg='black')
+    button3 = tkinter.Button(root1, text="3.EMPLOYEE REGISTRATION",command=reg,bg='light blue',fg='black')
+    button4 = tkinter.Button(root1, text="4.VIEW EMPLOYEE RECORDS",command=func5,bg='light green',fg='black')
+    #button5 = tkinter.Button(root1, text="5.SEARCH BY ADHAAR",command=submit_search,bg='light blue',fg='black')
+    button6 = tkinter.Button(root1, text="5.EXIT",command=ex,bg='light green',fg='black')
     m.place(x=75,y=5)
     button1.pack(side=tkinter.TOP)
     button1.place(x=80,y=50)
+    button2.pack(side=tkinter.TOP)
+    button2.place(x=80,y=100)
     button3.pack(side=tkinter.TOP)
-    button3.place(x=80,y=100)
+    button3.place(x=80,y=150)
     button4.pack(side=tkinter.TOP)
-    button4.place(x=80, y=150)
+    button4.place(x=80, y=200)
+    #button5.pack(side=tkinter.TOP)
+    #button5.place(x=80,y=250)
     button6.pack(side=tkinter.TOP)
-    button6.place(x=80,y=200)
+    button6.place(x=80,y=250)
     root1.mainloop()
 
